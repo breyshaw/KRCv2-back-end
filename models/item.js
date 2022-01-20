6c5d6b6708d6140057facb02adf7e09707fe3350
breyshaw <dawsonbrey@gmail.com>
2022-01-20 15:25:47 -0500

    create item model and review schema

---
+++ models/item.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const reviewSchema = new Schema({
+    user: { type: Schema.Types.ObjectId, ref: 'Profile' },
+    username: String,
+    content: String,
+    rating: { type: Number, min: 1, max: 5 },
+}, {
+    timestamps: true
+})
+
+const itemSchema = new Schema({
+    title: String,
+    imageUrl: String,
+    type: String,
+    price: Number,
+    info: String,
+    reviews: [reviewSchema]
+})
+
+const Item = mongoose.model('Item', itemSchema)
+
+
+export {
+    Item,
+}
